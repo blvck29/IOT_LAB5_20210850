19c5bbb2dbfa264ad0a728d8056b36f5977bbe45
blvck29 <a20210850@pucp.edu.pe>
2024-10-26 17:23:22 -0500

    add foods estatico

--- LAB5_IOT/app/src/main/java/com/app/lab5_iot/AppActivity.java
+++ LAB5_IOT/app/src/main/java/com/app/lab5_iot/AppActivity.java
@@ -2,6 +2,12 @@ package com.app.lab5_iot;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.RadioButton;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,13 +16,26 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 import com.app.lab5_iot.model.CircularTMB;
+import com.app.lab5_iot.model.DataCalorias;
 import com.app.lab5_iot.model.DataUsuario;
 
 public class AppActivity extends AppCompatActivity {
 
-
     private CircularTMB circularTMBProgressView;
     private DataUsuario dataUsuario;
+    private DataCalorias dataCalorias;
+
+    private Button inputCalorias;
+    private Button inputEjercicio;
+
+    private ImageButton configurarNotificaciones;
+
+    private Button addFood1;
+    private Button addFood2;
+    private Button addFood3;
+    private Button addFood4;
+    private Button addFood5;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,22 +50,80 @@ public class AppActivity extends AppCompatActivity {
 
         circularTMBProgressView = findViewById(R.id.circularTMBProgressView);
 
+        inputCalorias = findViewById(R.id.input_calorias);
+        inputEjercicio = findViewById(R.id.input_ejercicio);
+        configurarNotificaciones = findViewById(R.id.configurar_notificaciones);
+
+        addFood1 = findViewById(R.id.add_food1);
+        addFood2 = findViewById(R.id.add_food2);
+        addFood3 = findViewById(R.id.add_food3);
+        addFood4 = findViewById(R.id.add_food4);
+        addFood5 = findViewById(R.id.add_food5);
+
         Intent intent = getIntent();
         Bundle bundle = intent.getExtras();
 
-
         if (bundle != null) {
             dataUsuario = (DataUsuario) bundle.getSerializable("dataUsuario");
+            dataCalorias = new DataCalorias();
+            dataCalorias.setCaloriasConsumidas(0F);
+            dataCalorias.setCaloriasTotales(dataUsuario.getTMB());
         }
 
-        configureCircularTMBProgressView(0, dataUsuario.getTMB());
+        configureCircularTMBProgressView(dataCalorias);
 
+
+        // INICIA LA LOGICA
+
+
+        addFood1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dataCalorias.setCaloriasConsumidas(dataCalorias.getCaloriasConsumidas() + 942F);
+                configureCircularTMBProgressView(dataCalorias);
+            }
+        });
+
+        addFood2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dataCalorias.setCaloriasConsumidas(dataCalorias.getCaloriasConsumidas() + 1242F);
+                configureCircularTMBProgressView(dataCalorias);
+            }
+        });
+
+        addFood3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dataCalorias.setCaloriasConsumidas(dataCalorias.getCaloriasConsumidas() + 1062F);
+                configureCircularTMBProgressView(dataCalorias);
+            }
+        });
+
+        addFood4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dataCalorias.setCaloriasConsumidas(dataCalorias.getCaloriasConsumidas() + 1542F);
+                configureCircularTMBProgressView(dataCalorias);
+            }
+        });
+
+        addFood5.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dataCalorias.setCaloriasConsumidas(dataCalorias.getCaloriasConsumidas() + 966F);
+                configureCircularTMBProgressView(dataCalorias);
+            }
+        });
 
     }
 
+    private void configureCircularTMBProgressView(DataCalorias dataCalorias) {
 
-    private void configureCircularTMBProgressView(int actualData, Float totalData){
-        int actual = actualData;
+        Float actualData = dataCalorias.getCaloriasConsumidas();
+        Float totalData = dataCalorias.getCaloriasTotales();
+
+        Float actual = actualData;
         Float total = totalData;
         int totalSinDecimales = total.intValue();
         float porcentaje = (actual * 100f) / totalSinDecimales;
@@ -54,8 +131,4 @@ public class AppActivity extends AppCompatActivity {
 
         circularTMBProgressView.setPorcentaje(porcentaje, textCenter);
     }
-
-
-
-
 }
--- LAB5_IOT/app/src/main/java/com/app/lab5_iot/model/DataCalorias.java
+++ LAB5_IOT/app/src/main/java/com/app/lab5_iot/model/DataCalorias.java
@@ -1,4 +1,23 @@
 package com.app.lab5_iot.model;
 
 public class DataCalorias {
+
+    private Float caloriasConsumidas;
+    private Float caloriasTotales;
+
+    public Float getCaloriasConsumidas() {
+        return caloriasConsumidas;
+    }
+
+    public void setCaloriasConsumidas(Float caloriasConsumidas) {
+        this.caloriasConsumidas = caloriasConsumidas;
+    }
+
+    public Float getCaloriasTotales() {
+        return caloriasTotales;
+    }
+
+    public void setCaloriasTotales(Float caloriasTotales) {
+        this.caloriasTotales = caloriasTotales;
+    }
 }
